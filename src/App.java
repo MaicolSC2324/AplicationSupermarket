b68bef4f873e4115f0108d55bad12e2f504d56c6
MaicolSC2324 <mc43863@gmail.com>
2023-10-07 10:03:23 -0500

    FInal programa Promedio de Notas

--- src/App.java
+++ src/App.java
@@ -1,6 +1,7 @@
 // Librería para limpiar la consola o el Terminal donde se ejecuta el código
 import java.io.IOException;
 
+import javax.crypto.spec.DESKeySpec;
 import javax.swing.JOptionPane; 
 
 public class App {
@@ -20,13 +21,35 @@ public class App {
 
         // Declaramos variables
         String StudentName = "", Signature = "";
-        long [] Nota = new long[2], Porcentaje = new long[2];
-
-
+        int LargoNotas = 0;
+        double Calificacion = 0;
+        
         JOptionPane.showMessageDialog(null, "¡Welcome! in this app each student can to managment your calificatios by assignature.");
         StudentName = JOptionPane.showInputDialog(null, "Plis, write your full name");
         Signature = JOptionPane.showInputDialog(null, "Now write the signatures name");
         JOptionPane.showMessageDialog(null, "The name writed its " + StudentName + " and the signature writed its " + Signature);
+        LargoNotas = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el número de notas que va a ingresar"));
+
+        if (LargoNotas != 0){
+            double [] Nota = new double[LargoNotas], Porcentaje = new double[LargoNotas];
+
+            for (int i = 0; i < Porcentaje.length; i++) {
+                Nota[i] = Double.parseDouble(JOptionPane.showInputDialog("Ingrese la calificación número " + (i+1)));
+                Porcentaje[i] = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el porcentaje de la calificación número. \n Se entiende quesi escribe 40, equivale al 40% " + (i+1)));
+            }
+
+            for (int i = 0; i < Porcentaje.length; i++) {
+                Calificacion = Calificacion + (Nota[i] * Porcentaje[i] / 100);
+            }
+
+            JOptionPane.showMessageDialog(null, "Listo " + StudentName + ", Para la asiganuta " + Signature + " tenemos el siguiente resultado \nLa calificación final de su asignatura es: " + Calificacion);
+
+        }
+        
+
+
+
+        
         
         //Integer.parseInt(JOptionPane.showInputDialog("Ingrese un número entre " + LimMenor + " y " + LimMayor));
         //hola
