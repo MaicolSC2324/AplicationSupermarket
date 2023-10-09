6053a889353a6d03b6ffa27f4f036ff69dcf65e1
MaicolSC2324 <mc43863@gmail.com>
2023-10-08 22:26:57 -0500

    Primera Clase POO, Creación de Clase, Atributos, Getters, Setters y Métodos

--- src/App.java
+++ src/App.java
@@ -1,44 +1,19 @@
-import java.util.ArrayList;
-
-
 public class App {
     
     public static void main(String[] args)  {
-        /*
-            Dato Flotante float = 45.5F, siempre después de digitar el numero de la variable debe ir la F por sintaxis en Java
-            EnteroLargo long = 123.3L, siempre después de digitar el numero de la variable debe ir la L por sintaxis en Java
-        */
+        /* Instanciar una clase */
+        //----- Objeto miPersonaje de tipo Superpersonaje -----//
+        Superpersonaje miPersonaje = new Superpersonaje("Maicol Sánchez", "Invisibilidad", 1450000, false);
         
-        //Declaración de variables
-        Integer ValorMax, ValorMin, Suma = 0;
-        Integer Promedio;
-        ArrayList<Integer> myArrayDinamico = new ArrayList<Integer>();
-        myArrayDinamico.add(17);
-        myArrayDinamico.add(24);
-        myArrayDinamico.add(5);
-        myArrayDinamico.add(13);
-        myArrayDinamico.add(7);
-               
-        System.out.println(myArrayDinamico.size());
-        ValorMax = myArrayDinamico.get(0);
-        ValorMin = myArrayDinamico.get(0);
-        System.out.println("Maximo: " + ValorMax + "\nMinimo: " + ValorMin);
-        
-        
-        for (int i = 0; i < myArrayDinamico.size(); i++) {
-            if (myArrayDinamico.get(i) > ValorMax) {
-                ValorMax = myArrayDinamico.get(i);
-            }
+        System.out.println("El Super Personaje " + miPersonaje.getNombre() + " tiene el poder de " + miPersonaje.getPoder());
+        System.out.println("Valor COmercial de " + miPersonaje.getValorComercial());    
 
-            if (myArrayDinamico.get(i) < ValorMin){
-                ValorMin = myArrayDinamico.get(i);
-            }
-            Suma += myArrayDinamico.get(i);
-        }
-        Promedio = Suma / myArrayDinamico.size();
-        System.out.println("Valor máximo: " + ValorMax);
-        System.out.println("Valor mínimo: " + ValorMin);
-        System.out.println("Suma: " + Suma);
-        System.out.println("Promedio: " + Promedio);
+        miPersonaje.setValorComercial(-38);
+        System.out.println("primer set: " + miPersonaje.getValorComercial());
+
+        miPersonaje.setValorComercial(1900000);
+        System.out.println("segundo set: " + miPersonaje.getValorComercial());
+
+        miPersonaje.correr();
     }
 }
