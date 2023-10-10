37d814b719748047d770132d2478a917060b248a
MaicolSC2324 <mc43863@gmail.com>
2023-10-09 22:21:24 -0500

    POO explicacion de Sobrecargar métodos

--- src/App.java
+++ src/App.java
@@ -1,19 +1,11 @@
 public class App {
     
     public static void main(String[] args)  {
-        /* Instanciar una clase */
-        //----- Objeto miPersonaje de tipo Superpersonaje -----//
-        Superpersonaje miPersonaje = new Superpersonaje("Maicol Sánchez", "Invisibilidad", 1450000, false);
         
-        System.out.println("El Super Personaje " + miPersonaje.getNombre() + " tiene el poder de " + miPersonaje.getPoder());
-        System.out.println("Valor COmercial de " + miPersonaje.getValorComercial());    
+        MoldePokemon miPokemon = new MoldePokemon("Charmander", "Fuego");
+        System.out.println("El numero del prokemon es: " + miPokemon.getNumero());
 
-        miPersonaje.setValorComercial(-38);
-        System.out.println("primer set: " + miPersonaje.getValorComercial());
-
-        miPersonaje.setValorComercial(1900000);
-        System.out.println("segundo set: " + miPersonaje.getValorComercial());
-
-        miPersonaje.correr();
+        System.out.println("Probando Método to String: " + miPokemon.toString()); 
+                
     }
 }
