713c3b14f6fd364374ba6fc320d7c04e74c4c76a
MaicolSC2324 <mc43863@gmail.com>
2023-10-12 23:06:09 -0500

    iniciando nueva ckase java

--- src/App.java
+++ src/App.java
@@ -1,12 +1,8 @@
-import javax.swing.JOptionPane;
-
 public class App {
     
     public static void main(String[] args)  {
         
-        MoldePokemon miPokemon = new MoldePokemon("Charmander", "Fuego");
-        MoldePokemon otroPokemon = new MoldePokemon("Venusaur", "Planta");
-        JOptionPane.showMessageDialog(null, miPokemon.luchar(otroPokemon, "Lanzallamas"), null, 0, null);
+        
                         
     }
 }
